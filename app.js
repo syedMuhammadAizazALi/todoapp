32976014b618990ac09f9cb4c566b3f06d11daef
Syed Muhammad Aizaz Ali <136581038+syedMuhammadAizazALi@users.noreply.github.com>
2023-08-31 03:26:20 -0700

    ok

--- app.js
+++ app.js
@@ -1,18 +1,62 @@
-const input =document.querySelector("input")
+const input = document.querySelector("input")
+const ul = document.querySelector("ul")
+const errorMessage = document.querySelector("#error-message"); 
 let todo = []
 function sends() {
-    if(input.value==''){
-     alert('please enter task')
+    if (input.value == '') {
+        errorMessage.textContent = 'Please enter a task'; 
     }
-    else{
-    console.log(input.value);
-    input.value=''
-    todo.push(input);
-    console.log(todo);
-    for (let i = 0; i < todo.length; i++) {
-       ul.innerHTML= `<li>${todo} </li>`
-            
+    else {
+
+        console.log(input.value);
+        todo.push(input.value);
+        console.log(todo);
+        input.value = ''
+        ul.innerHTML = "";
+        for (let i = 0; i < todo.length; i++) {
+            const list = todo[i];
+            ul.innerHTML += `<li>${list}</li><button  onclick="deleteTask(${i})" >Delete</button>
+                <button onclick="editTask(${i})">Edit</button> `
+        }
+
     }
 }
+function deleteTask(index) {
+    todo.splice(index, 1); 
+    renderTasks(); 
+}
+
+function editTask(index) {
+    const newText = prompt('Edit task:', todo[index]);
+    if (newText !== null) {
+        todo[index] = newText;
+        renderTasks();
     }
+}
+
+function renderTasks() {
+    ul.innerHTML = "";
+    for (let i = 0; i < todo.length; i++) {
+        const list = todo[i];
+        ul.innerHTML += `
+            <li>${list}
+                <button onclick="deleteTask(${i})">Delete</button>
+                <button onclick="editTask(${i})">Edit</button>
+            </li>`;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
